8719bd7f0f7a85a5e151d0f2bf0bb1c488f8706a
111 <cer289126@gmail.com>
2020-08-30 22:33:29 +0800

    Row and Column

--- flutter_app/lib/main.dart
+++ flutter_app/lib/main.dart
@@ -13,17 +13,41 @@ class MyApp extends StatelessWidget {
       home: Scaffold(
         backgroundColor: Colors.blue,
         body: SafeArea(
-          child: Container(
-            margin: EdgeInsets.all(40),
-            padding: EdgeInsets.all(30),
-            height: 100,
-            width: 100,
-            color: Colors.white,
-            child: Text('Test'),
+          // наглядно показано тут https://medium.com/flutter-community/flutter-layout-cheat-sheet-5363348d037e
+          child: Row(
+//            mainAxisAlignment: MainAxisAlignment.spaceBetween ,// расположение между
+            // verticalDirection: VerticalDirection.up, снизу начинаются
+            crossAxisAlignment: CrossAxisAlignment.stretch, // по вертикали
+            children: [
+              Container(
+//                height: 100,
+//                width: double.infinity,
+                width: 30,
+                color: Colors.white,
+                child: Text('Container 1'),
+              ),
+              SizedBox(
+//                height: 20,
+                width: 20,
+              ),
+              Container(
+                height: 100,
+//                width: 100,
+//                width: double.infinity,
+                color: Colors.white60,
+                child: Text('Container 2'),
+              ),
+              Container(
+                height: 100,
+//                width: 100,
+//                width: double.infinity,
+                color: Colors.red,
+                child: Text('Container 3'),
+              ),
+            ],
           ),
         ),
       ),
     );
   }
 }
-

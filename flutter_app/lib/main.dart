18a1c067dee402ff1f188e6f3d40a8ed8a2a9728
111 <cer289126@gmail.com>
2020-09-02 15:15:53 +0800

    Игра_кубики(fullstatewidget,state,Кнопка с картинкой)

--- flutter_app/lib/main.dart
+++ flutter_app/lib/main.dart
@@ -1,137 +1,62 @@
-import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
+import 'dart:math';
 
 void main() {
-  runApp(
-    MyApp(),
+  return runApp(
+    MaterialApp(
+      home: Scaffold(
+        backgroundColor: Colors.red,
+        appBar: AppBar(
+          title: Text('Dicee'),
+          backgroundColor: Colors.red,
+        ),
+        body: DicePage(),
+      ),
+    ),
   );
 }
 
-class MyApp extends StatelessWidget {
+class DicePage extends StatefulWidget {
+  @override
+  _DicePageState createState() => _DicePageState();
+}
+
+class _DicePageState extends State<DicePage> {
+  int leftDiceNumber = 3;
+  int rightDiceNumber = 2;
+
+  void NumberDice() {
+    setState(() {
+      leftDiceNumber = Random().nextInt(6) + 1;
+      rightDiceNumber = Random().nextInt(6) + 1;
+    });
+  }
+
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(
-      home: Scaffold(
-        backgroundColor: Colors.teal,
-        body: SafeArea(
-          // наглядно показано тут https://medium.com/flutter-community/flutter-layout-cheat-sheet-5363348d037e
-          // цвета,иконки https://www.materialpalette.com/icons
-          // шрифты https://fonts.google.com/
-          child: Column(
-            mainAxisAlignment: MainAxisAlignment.center,
-            children: [
-              CircleAvatar(
-                radius: 50,
-                backgroundImage: AssetImage('images/avatar.jpg'),
-              ),
-              Text(
-                'Sergey Programm',
-                style: TextStyle(
-                  fontFamily: 'Pacifico',
-                  fontSize: 20,
-                  fontWeight: FontWeight.bold,
-                  color: Colors.white,
-                ),
-              ),
-              Text(
-                'FLUTTER DEVELOPER',
-                style: TextStyle(
-                  fontFamily: 'SansPro',
-                  fontSize: 10,
-                  fontWeight: FontWeight.bold,
-                  color: Colors.teal.shade100,
-                  letterSpacing: 2.0,
-                ),
-              ),
-              SizedBox(
-                height: 10,
-                width: 150,
-                child: Divider(
-                  color: Colors.teal.shade100,
-                ),
-              ),
-              Card(
-                color: Colors.white,
-                margin: EdgeInsets.symmetric(vertical: 20, horizontal: 20),
-//                padding: EdgeInsets.all(10),
-                child: ListTile(
-                  leading: Icon(
-                    Icons.phone,
-                    color: Colors.teal,
-                  ),
-                  title: Text(
-                    '+7 999-777-56-78',
-                    style: TextStyle(
-                      color: Colors.teal.shade900,
-                      fontFamily: 'SansPro',
-                      fontSize: 20,
-                    ),
-                  ),
-                ),
-              ),
-              Card(
-                color: Colors.white,
-                margin: EdgeInsets.symmetric(vertical: 20, horizontal: 20),
-//                padding: EdgeInsets.all(10),
-                child: ListTile(
-                  leading: Icon(
-                    Icons.email,
-                    color: Colors.teal,
-                  ),
-                  title: Text(
-                    'test@yandex.ru',
-                    style: TextStyle(
-                      color: Colors.teal.shade900,
-                      fontFamily: 'SansPro',
-                      fontSize: 20,
-                    ),
-                  ),
-                ),
-              ),
-            ],
+    return Center(
+      child: Row(
+        children: [
+          Expanded(
+            flex: 1,
+            child: FlatButton(
+                onPressed: () {
+                  NumberDice();
+                },
+                child:
+                    Image(image: AssetImage('images/dice$leftDiceNumber.png'))),
           ),
-        ),
+          Expanded(
+            flex: 1,
+            child: FlatButton(
+                onPressed: () {
+                  NumberDice();
+                },
+                child: Image(
+                    image: AssetImage('images/dice$rightDiceNumber.png'))),
+          ),
+        ],
       ),
     );
   }
 }
-
-//Row(
-//children: [
-//Icon(
-//Icons.phone,
-//color: Colors.teal,
-//),
-//SizedBox(
-//width: 20,
-//),
-//Text(
-//'+7 999-777-56-78',
-//style: TextStyle(
-//color: Colors.teal.shade900,
-//fontFamily: 'SansPro',
-//fontSize: 20,
-//),
-//),
-//],
-//),
-
-//child: Row(
-//children: [
-//Icon(
-//Icons.email,
-//color: Colors.teal,
-//),
-//SizedBox(
-//width: 20,
-//),
-//Text(
-//'test@yandex.ru',
-//style: TextStyle(
-//color: Colors.teal.shade900,
-//fontFamily: 'SansPro',
-//fontSize: 20,
-//),
-//),
-//],
-//),

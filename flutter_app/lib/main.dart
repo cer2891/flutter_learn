edff1b2b8624017bb209f2bb4a04f44b21621e4c
111 <cer289126@gmail.com>
2020-09-03 10:10:36 +0800

    Кселафон(audio play,FlatButton,Expended),функции применения

--- flutter_app/lib/main.dart
+++ flutter_app/lib/main.dart
@@ -1,61 +1,79 @@
 import 'package:flutter/material.dart';
-import 'dart:math';
+import 'package:audioplayers/audio_cache.dart';
 
-void main() {
-  return runApp(
-    MaterialApp(
-      home: Scaffold(
-        backgroundColor: Colors.red,
-        appBar: AppBar(
-          title: Text('Dicee'),
-          backgroundColor: Colors.red,
-        ),
-        body: DicePage(),
+void main() => runApp(XylophoneApp());
+
+class XylophoneApp extends StatelessWidget {
+  Expanded Key({int number, Color color}) {
+    return Expanded(
+      child: FlatButton(
+        color: color,
+        onPressed: () {
+          final player = AudioCache();
+          player.play('note$number.wav');
+        },
       ),
-    ),
-  );
-}
-
-class DicePage extends StatefulWidget {
-  @override
-  _DicePageState createState() => _DicePageState();
-}
-
-class _DicePageState extends State<DicePage> {
-  int leftDiceNumber = 3;
-  int rightDiceNumber = 2;
-
-  void NumberDice() {
-    setState(() {
-      leftDiceNumber = Random().nextInt(6) + 1;
-      rightDiceNumber = Random().nextInt(6) + 1;
-    });
+    );
   }
 
   @override
   Widget build(BuildContext context) {
-    return Center(
-      child: Row(
-        children: [
-          Expanded(
-            flex: 1,
-            child: FlatButton(
-                onPressed: () {
-                  NumberDice();
-                },
-                child:
-                    Image(image: AssetImage('images/dice$leftDiceNumber.png'))),
-          ),
-          Expanded(
-            flex: 1,
-            child: FlatButton(
-                onPressed: () {
-                  NumberDice();
-                },
-                child: Image(
-                    image: AssetImage('images/dice$rightDiceNumber.png'))),
-          ),
-        ],
+    return MaterialApp(
+      home: Scaffold(
+        body: SafeArea(
+          child:
+              Column(crossAxisAlignment: CrossAxisAlignment.stretch, children: [
+            Key(number: 1, color: Colors.red),
+            Key(number: 2, color: Colors.orange),
+            Key(number: 3, color: Colors.yellow),
+            Key(number: 4, color: Colors.green),
+            Key(number: 5, color: Colors.teal),
+            Key(number: 6, color: Colors.blue),
+            Key(number: 7, color: Colors.purpleAccent),
+//                  FlatButton(
+//                    color: Colors.red,
+//                    onPressed: () {
+//                      PlaySound(number: 1);
+//                    },
+//                  ),
+//                  FlatButton(
+//                    color: Colors.orange,
+//                    onPressed: () {
+//                      PlaySound(number: 2);
+//                    },
+//                  ),
+//                  FlatButton(
+//                    color: Colors.yellow,
+//                    onPressed: () {
+//                      PlaySound(number: 3);
+//                    },
+//                  ),
+//                  FlatButton(
+//                    color: Colors.green,
+//                    onPressed: () {
+//                      PlaySound(number: 4);
+//                    },
+//                  ),
+//                  FlatButton(
+//                    color: Colors.teal,
+//                    onPressed: () {
+//                      PlaySound(number: 5);
+//                    },
+//                  ),
+//                  FlatButton(
+//                    color: Colors.blue,
+//                    onPressed: () {
+//                      PlaySound(number: 6);
+//                    },
+//                  ),
+//                  FlatButton(
+//                    color: Colors.purpleAccent,
+//                    onPressed: () {
+//                      PlaySound(number: 7);
+//                    },
+//                  ),
+          ]),
+        ),
       ),
     );
   }

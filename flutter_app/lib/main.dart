70f2c25c55662f167b13d681f241f60e40910c88
111 <cer289126@gmail.com>
2020-09-17 08:17:43 +0800

    Опрос_классы_setState_иконки_и_пакетAlter

--- flutter_app/lib/main.dart
+++ flutter_app/lib/main.dart
@@ -1,80 +1,146 @@
+import 'package:Learn/questions.dart';
 import 'package:flutter/material.dart';
-import 'package:audioplayers/audio_cache.dart';
+import 'package:rflutter_alert/rflutter_alert.dart';
+// ignore: avoid_web_libraries_in_flutter
+//import 'dart:js';
 
-void main() => runApp(XylophoneApp());
+void main() => runApp(Quizzler());
 
-class XylophoneApp extends StatelessWidget {
-  Expanded Key({int number, Color color}) {
-    return Expanded(
-      child: FlatButton(
-        color: color,
-        onPressed: () {
-          final player = AudioCache();
-          player.play('note$number.wav');
-        },
-      ),
-    );
-  }
-
+class Quizzler extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
       home: Scaffold(
+        backgroundColor: Colors.grey.shade900,
         body: SafeArea(
-          child:
-              Column(crossAxisAlignment: CrossAxisAlignment.stretch, children: [
-            Key(number: 1, color: Colors.red),
-            Key(number: 2, color: Colors.orange),
-            Key(number: 3, color: Colors.yellow),
-            Key(number: 4, color: Colors.green),
-            Key(number: 5, color: Colors.teal),
-            Key(number: 6, color: Colors.blue),
-            Key(number: 7, color: Colors.purpleAccent),
-//                  FlatButton(
-//                    color: Colors.red,
-//                    onPressed: () {
-//                      PlaySound(number: 1);
-//                    },
-//                  ),
-//                  FlatButton(
-//                    color: Colors.orange,
-//                    onPressed: () {
-//                      PlaySound(number: 2);
-//                    },
-//                  ),
-//                  FlatButton(
-//                    color: Colors.yellow,
-//                    onPressed: () {
-//                      PlaySound(number: 3);
-//                    },
-//                  ),
-//                  FlatButton(
-//                    color: Colors.green,
-//                    onPressed: () {
-//                      PlaySound(number: 4);
-//                    },
-//                  ),
-//                  FlatButton(
-//                    color: Colors.teal,
-//                    onPressed: () {
-//                      PlaySound(number: 5);
-//                    },
-//                  ),
-//                  FlatButton(
-//                    color: Colors.blue,
-//                    onPressed: () {
-//                      PlaySound(number: 6);
-//                    },
-//                  ),
-//                  FlatButton(
-//                    color: Colors.purpleAccent,
-//                    onPressed: () {
-//                      PlaySound(number: 7);
-//                    },
-//                  ),
-          ]),
+          child: Padding(
+            padding: EdgeInsets.symmetric(horizontal: 10.0),
+            child: QuizPage(),
+          ),
         ),
       ),
     );
   }
 }
+
+class QuizPage extends StatefulWidget {
+  @override
+  _QuizPageState createState() => _QuizPageState();
+}
+
+class _QuizPageState extends State<QuizPage> {
+  List<Icon> scopeKeeper = [];
+  QuizBrain quizBrain = QuizBrain();
+  // List<Question> questionBank=[
+  //   Question(q: 'You can lead a cow down stairs but not up stairs.',a: false),
+  //   Question(q: 'Approximately one quarter of human bones are in the feet.',a: true),
+  //   Question(q: 'A slug\'s blood is green.',a: true),
+  // ];
+
+  void checkAnswer(bool userPickAnswer)
+  {
+    quizBrain.nextNumber();
+    setState(() {
+
+      if(quizBrain.isFinished()==true)
+        {
+          Alert(
+            context: context,
+            title: 'Усе',
+            desc: 'Конец теста',
+          ).show();
+
+          //TODO Step 4 Part C - reset the questionNumber,
+          quizBrain.reset();
+
+          //TODO Step 4 Part D - empty out the scoreKeeper.
+          scopeKeeper = [];
+        }
+      else {
+        if (quizBrain.getQuestionAnswer() == userPickAnswer) {
+          scopeKeeper.add(Icon(
+            Icons.check,
+            color: Colors.green,
+          ));
+        } else {
+          scopeKeeper.add(Icon(
+            Icons.close,
+            color: Colors.red,
+          ));
+        }
+      }
+    });
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Column(
+      mainAxisAlignment: MainAxisAlignment.spaceBetween,
+      crossAxisAlignment: CrossAxisAlignment.stretch,
+      children: <Widget>[
+        Expanded(
+          flex: 5,
+          child: Padding(
+            padding: EdgeInsets.all(10.0),
+            child: Center(
+              child: Text(
+                quizBrain.getQuestionText(),
+                textAlign: TextAlign.center,
+                style: TextStyle(
+                  fontSize: 25.0,
+                  color: Colors.white,
+                ),
+              ),
+            ),
+          ),
+        ),
+        Expanded(
+          child: Padding(
+            padding: EdgeInsets.all(15.0),
+            child: FlatButton(
+              textColor: Colors.white,
+              color: Colors.green,
+              child: Text(
+                'True',
+                style: TextStyle(
+                  color: Colors.white,
+                  fontSize: 20.0,
+                ),
+              ),
+              onPressed: () {
+                  checkAnswer(true);
+                },
+            ),
+          ),
+        ),
+        Expanded(
+          child: Padding(
+            padding: EdgeInsets.all(15.0),
+            child: FlatButton(
+              color: Colors.red,
+              child: Text(
+                'False',
+                style: TextStyle(
+                  fontSize: 20.0,
+                  color: Colors.white,
+                ),
+              ),
+              onPressed: () {
+                checkAnswer(false);
+              },
+            ),
+          ),
+        ),
+        Row(
+          children: scopeKeeper,
+        ),
+      ],
+    );
+  }
+}
+
+/*
+question1: 'You can lead a cow down stairs but not up stairs.', false,
+question2: 'Approximately one quarter of human bones are in the feet.', true,
+question3: 'A slug\'s blood is green.', true,
+*/
